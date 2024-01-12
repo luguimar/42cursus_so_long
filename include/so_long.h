f34abe79c4a3430f1ad2b68f8f968701d138db00
Luis Gustavo Guimaraes Cavalcante Maia De Sousa <luguimar@c1r14s2.42porto.com>
2024-01-12 23:19:36 +0000

    updated so_long.h

--- include/so_long.h
+++ include/so_long.h
@@ -6,14 +6,23 @@
 /*   By: luguimar <luguimar@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 22:28:32 by luguimar          #+#    #+#             */
-/*   Updated: 2024/01/10 22:31:29 by luguimar         ###   ########.fr       */
+/*   Updated: 2024/01/12 23:17:46 by luguimar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
-# define SO_LONG_H
+#define SO_LONG_H
 
-# include "../lib/libft/libft.h"
-# include "../lib/mlx/mlx.h"
+#include "../lib/libft/libft.h"
+#include "../lib/mlx/mlx.h"
+
+typedef struct s_game
+{
+	int p_x;
+	int p_y;
+	void *mlx;
+	void *win;
+
+} t_game;
 
 #endif
